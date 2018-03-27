f93c942517def5d07aaefaa3862ec669cf9fdafb
abshir_jr <abshir_jr@github.com>
2018-03-27 22:38:00 +0100

    started working on problem 3

--- Problem3/src/com/abshirahmed/Main.java
+++ Problem3/src/com/abshirahmed/Main.java
@@ -1,37 +1,45 @@
 package com.abshirahmed;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 
-/**
- * The prime factors of 13195 are 5, 7, 13 and 29.
- * What is the largest prime factor of the number 600851475143 ?
- */
+import java.util.ArrayList;
 
 public class Main {
-
+    /**
+     * The prime factors of 13195 are 5, 7, 13 and 29.
+     * What is the largest prime factor of the number 600851475143 ?
+     * <p>
+     * <p>1. get a value</p>
+     * <p>2. check if that value is divisible by a prime number</p>
+     * <p>3. store that prime factor in an ArrayList</p>
+     * <p>4. use answer as reference</p>
+     * <p>5. repeat steps 2 to 4</p>
+     * <p>6. repeat until answer equals 1</p>
+     */
     public static void main(String[] args) {
-
-        int limit = 10;
-        System.out.println(Arrays.asList(getPrimeNumbersList(limit).toArray()));
+        int value = 24325;
+        findPrimeFactorsOf(value);
     }
 
+    private static int findPrimeFactorsOf(int value) {
 
-    private static ArrayList<Integer> getPrimeNumbersList(int limit) {
-        ArrayList<Integer> primeNumbersList = new ArrayList<>();
+        ArrayList<Integer> primeFactors = new ArrayList<>();
+        ArrayList<Integer> newValue = new ArrayList<>();
 
-        for (int i = 2; i < limit; i++) {
+        for (int i = 1; i <= value; i++) {
 
-            for (int j = 1; j < i; j++) {
-
-                if (((i / j == 1) || (i / j == i)) & (j % i != 0)) {
-                    primeNumbersList.add(i);
-                }
-
+            if ((value % i == 0) & (i != 1)) {
+                primeFactors.add(i);
+                newValue.add(value / i);
+                System.out.println(i+" is a prime factor");
+                return findPrimeFactorsOf(value / i);
             }
 
         }
+        return 0;
 
-        return primeNumbersList;
     }
+
+
 }
+
+
