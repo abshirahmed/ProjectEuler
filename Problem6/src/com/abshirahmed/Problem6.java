d9a6fc5141558d3045383b1a6abb92335e04fd06
Abshir <abshir.ahmed@outlook.com>
2018-04-03 21:57:54 +0100

    completed problem 6 new

--- Problem6/src/com/abshirahmed/Problem6.java
+++ Problem6/src/com/abshirahmed/Problem6.java
@@ -4,23 +4,22 @@ public class Problem6 {
 
     public static void main(String[] args) {
 
-        int squareOfSums = 0, sumOfSquares = 0, difference = 0;
+        int squareOfSums = 0, sumOfSquares = 0, difference;
 
         for (int i = 1; i <= 100; i++) {
             squareOfSums += i;
             sumOfSquares += (i * i);
-            if (i == 100) {
-                squareOfSums *= squareOfSums;
-                if (squareOfSums > sumOfSquares) {
-                    difference = squareOfSums - sumOfSquares;
-                } else difference = sumOfSquares - squareOfSums;
-            }
         }
+
+        squareOfSums *= squareOfSums;
+        difference = (squareOfSums > sumOfSquares) ? (squareOfSums - sumOfSquares) : (sumOfSquares - squareOfSums);
+
         System.out.println("***************************************");
         System.out.println("square of sums = " + squareOfSums);
         System.out.println("sum of squares = " + sumOfSquares);
         System.out.println("***************************************");
         System.out.println("squareOfSums - sumOfSquares = " + difference);
         System.out.println("***************************************");
+
     }
 }
